13737c71ae7dad288b2468e7588edc27c25d2118
cloud <summerisfirefly@gmail.com>
2024-10-23 14:11:02 +0800

    Delete unnecessary run.sh file

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ] || [[ $1 != src* ]]; then
-    echo "Usage: $0 src/experiment-filename"
-    exit 1
-fi
-cppFile="$1"
-g++ $cppFile -o a.out -std=c++17 && ./a.out
-rm a.out
